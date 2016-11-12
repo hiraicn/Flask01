090ce5c7dab4e75a54067e61396244eaa023a4be
hiraicn <hiraicn@gmail.com>
2016-11-12 22:32:12 +0800

    test1

--- Flask01.py
+++ Flask01.py
@@ -1,12 +1,36 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+
+#!/usr/bin/python
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'index'
 
-@app.route('/user/<user>')
-def hello_world(user):
-    return 'Hello %s' % user
+#使用<user>传递参数
+@app.route('/hello/<user>')
+def hello_get(user):
+    return 'hello get %s' % user
 
+#使用POST请求
+@app.route('/hello/<user>', methods=['POST'])
+def hello_post(user):
+    return 'hello post %s' % user
+
+@app.route('/hotCity')
+def hotCity():
+    cities = ['北京', '上海', '广州']
+    return jsonify({
+            'code': 0, 
+            'cities': cities,
+        })
 
 if __name__ == '__main__':
     app.run()
+
+
+#还可以浏览器调试 
+#    app.run(debug=True)
