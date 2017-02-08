fb0592b676100d3b3ecb6c7cd94a4725c5ae1a96
hiraicn <hiraicn@gmail.com>
2017-02-08 13:13:26 +0800

    第三节 2. 构建 URL

--- Flask01.py
+++ Flask01.py
@@ -22,5 +22,13 @@ def show_user_profile(username):
 def show_post(post_id):
     return 'Post %d' % post_id      # 显示提交整型的用户"id"的结果，注意"int"是将输入的字符串形式转换为整型数据
 
+@app.route('/project/')     #不建议这种写法
+def project():
+    return 'The project page'
+
+@app.route('/about')        # 斜线只可少写不可多写，有助于避免搜索引擎索引同一个页面两次
+def about():
+    return 'The about page'
+
 if __name__ == '__main__':
     app.run(debug=True)
