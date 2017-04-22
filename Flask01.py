fbb1bbe9fa7fda058b9bb452ff0d004b02a63923
hiraicn <hiraicn@gmail.com>
2017-04-22 20:26:37 +0800

    update

--- Flask01.py
+++ Flask01.py
@@ -2,8 +2,7 @@
 
 #!/usr/bin/python
 
-from flask import Flask,render_template
-
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
@@ -11,14 +10,19 @@ app = Flask(__name__)
 def index():
     return 'Index Page'
 
+@app.route('/agent')
+def agent():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' % user_agent
+
 @app.route('/hello')
 def hello_world():
-    return 'Hello World!'
+    return '<h1>Hello World!</h1>'
 
 # 显示用户名称
 @app.route('/user/<username>')
 def show_user_profile(username):
-    return 'User %s' % username
+    return 'User: %s' % username
 
 # 显示提交整型的用户"id"的结果，注意"int"是将输入的字符串形式转换为整型数据
 @app.route('/post/<int:post_id>')
@@ -47,10 +51,23 @@ def login():
 @app.route('/sum/<int:a>/<int:b>')
 def sum(a,b):
     return '%d' %(a+b)
+
 # 第4节习题
 @app.route('/lesson4')
 def lesson4():
     return render_template('lesson4.html')
+
+# @app.route('/login', methods=['POST', 'GET'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         if valid_login(request.form['username'],
+#                        request.form['password']):
+#             return log_the_user_in(request.form['username'])
+#         else:
+#             error = 'Invalid username/password'
+#     # 当请求形式为“GET”或者认证失败则执行以下代码
+#     return render_template('login.html', error=error)
 
 if __name__ == '__main__':
     app.run(debug=True)
